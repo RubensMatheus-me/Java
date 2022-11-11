01fcf1bb5f3d53daca17b2188daf4bcc534cd79c
Rubens Matheus <y2afepolfxx@gmail.com>
2022-11-11 11:50:23 -0300

    commit 3

--- xadrezProject/src/App/Program.java
+++ xadrezProject/src/App/Program.java
@@ -1,14 +1,11 @@
 package App;
 
-import boardgame.Board;
-import boardgame.Position;
+import chess.ChessMatch;
 
 public class Program {
     public static void main(String[] args) {
 
-        System.out.println("Hello, World!");
-
-        Board board = new Board(8, 8);
-        System.out.println(board);
+        ChessMatch chessMatch = new ChessMatch();
+        UI.printBoard(chessMatch.getPieces());
     }
 }
