f7d7fd52556537861445603d4b47fcb86028104d
Rubens Matheus <y2afepolfxx@gmail.com>
2022-11-11 11:21:03 -0300

    commit 2

--- xadrezProject/src/App/Program.java
+++ xadrezProject/src/App/Program.java
@@ -1,5 +1,6 @@
 package App;
 
+import boardgame.Board;
 import boardgame.Position;
 
 public class Program {
@@ -7,7 +8,7 @@ public class Program {
 
         System.out.println("Hello, World!");
 
-        Position pos = new Position(3, 5);
-        System.out.println(pos);
+        Board board = new Board(8, 8);
+        System.out.println(board);
     }
 }
